9f26afea40f9a0cb8a17985fe5066437a9d91464
souravirus <souravsahoo66@yahoo.com>
2019-03-07 18:51:20 +0530

    Added typeahead.js package to the javascript assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,6 +42,7 @@
 //= require editor.js
 //= require like.js
 //= require main_image.js
+//= require typeahead.js/dist/typeahead.bundle.js
 //= require restful_typeahead.js
 //= require users.js
 //= require searchform.js
